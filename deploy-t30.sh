bc6d0e4d4446d89fbc6010de611d7131c6041d19
maddadder <rleecharlie@gmail.com>
2023-08-24 18:42:12 -0700

    initial commit

---
+++ deploy-t30.sh
@@ -0,0 +1,8 @@
+docker-compose build
+docker push 192.168.1.151:32000/payplh:1.11.101
+
+#payplh-t30
+helm upgrade payplh -f ./chart/values.yaml ./chart --namespace default
+
+#dashboard
+kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443
